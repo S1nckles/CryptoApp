add20bb2c42df4f66ef2f946f45f1ececf1e4a0c
Andrey <ilynych0810@gmail.com>
2024-09-08 13:26:45 +0200

    Adding a dynamic table

--- src/components/layout/AssetsTabel.jsx
+++ src/components/layout/AssetsTabel.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { Table } from 'antd';
+import CryptoContext from '../../context/CryptoContext';
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    sorter: (a, b) => a.name.length - b.name.length,
+  },
+  {
+    title: 'Price, $',
+    dataIndex: 'price',
+    sorter: (a, b) => a.price - b.price,
+  },
+  {
+    title: 'Amount',
+    dataIndex: 'amount',
+    defaultSortOrder: 'descend',
+    sorter: (a, b) => a.amount - b.amount,
+  },
+];
 
 export const AssetsTabel = () => {
+    const {assets} = useContext(CryptoContext);
+
+    const data = assets.map(a => ({
+        key: a.id,
+        name: a.name,
+        price: a.price,
+        amount: a.amount,
+    }))
+    
     return (
-        <div>
-            Tabel
-        </div>
+        <Table
+            columns={columns}
+            dataSource={data}
+            pagination={false}
+        />
     );
 };
